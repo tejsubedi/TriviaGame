329bfeab33f4990aa72d120b6fe21940f12f7a90
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-07-10 22:05:19 -0400

    array of object created

---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+let questions = [
+    {"question": "The first mechanical computer designed by Charles Babage was called ?",
+    "option1": "Abacus",
+    "option2": "Analytical engine",
+    "option3": "calculator",
+    "option4": "processor",
+    "answer": "2"
+}, {"question": "The first mechanical computer designed by Charles Babage was called ?",
+"option1": "Abacus",
+"option2": "Analytical engine",
+"option3": "calculator",
+"option4": "processor",
+"answer": "2"
+},{"question": "The first mechanical computer designed by Charles Babage was called ?",
+"option1": "Abacus",
+"option2": "Analytical engine",
+"option3": "calculator",
+"option4": "processor",
+"answer": "2"
+},{"question": "The first mechanical computer designed by Charles Babage was called ?",
+"option1": "Abacus",
+"option2": "Analytical engine",
+"option3": "calculator",
+"option4": "processor",
+"answer": "2"
+},{"question": "The first mechanical computer designed by Charles Babage was called ?",
+"option1": "Abacus",
+"option2": "Analytical engine",
+"option3": "calculator",
+"option4": "processor",
+"answer": "2"
+},
+]
+
+$(document).ready(() =>{
+    var $start = $("#start");
+
+});
+var currentQuestion = 0;
+var score = 0;
+var totalQuestions = questions.length;
+
+console.log(questions[0]);
