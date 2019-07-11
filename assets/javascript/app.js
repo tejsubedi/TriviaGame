240bc624dc049b3caf080fc99616cdedf2950d17
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-07-11 03:36:28 -0400

    array of object created

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,70 @@
+//Global Variables
+const question = $("#question");
+
+//create questions
 let questions = [
-    {"question": "The first mechanical computer designed by Charles Babage was called ?",
-    "option1": "Abacus",
-    "option2": "Analytical engine",
-    "option3": "calculator",
-    "option4": "processor",
-    "answer": "2"
-}, {"question": "The first mechanical computer designed by Charles Babage was called ?",
-"option1": "Abacus",
-"option2": "Analytical engine",
-"option3": "calculator",
-"option4": "processor",
-"answer": "2"
-},{"question": "The first mechanical computer designed by Charles Babage was called ?",
-"option1": "Abacus",
-"option2": "Analytical engine",
-"option3": "calculator",
-"option4": "processor",
-"answer": "2"
-},{"question": "The first mechanical computer designed by Charles Babage was called ?",
-"option1": "Abacus",
-"option2": "Analytical engine",
-"option3": "calculator",
-"option4": "processor",
-"answer": "2"
-},{"question": "The first mechanical computer designed by Charles Babage was called ?",
-"option1": "Abacus",
-"option2": "Analytical engine",
-"option3": "calculator",
-"option4": "processor",
-"answer": "2"
-},
+    {
+        question: "The first mechanical computer designed by Charles Babage was called ?",
+        choice0: "Abacus",
+        choice1: "Analytical engine",
+        choice2: "calculator",
+        choice3: "processor",
+        imgSrc: "assets/images/favicon.ico",
+        correct: "2"
+    }, {
+        question: "The first mechanical computer designed by Charles Babage was called ?",
+        choice0: "Abacus",
+        choice1: "Analytical engine",
+        choice2: "calculator",
+        choice3: "processor",
+        imgSrc: "assets/images/favicon.ico",
+        correct: "1"
+    }, {
+        question: "The first mechanical computer designed by Charles Babage was called ?",
+        choice0: "Abacus",
+        choice1: "Analytical engine",
+        choice2: "calculator",
+        choice3: "processor",
+        imgSrc: "assets/images/favicon.ico",
+        correct: "3"
+    }, {
+        question: "The first mechanical computer designed by Charles Babage was called ?",
+        choice0: "Abacus",
+        choice1: "Analytical engine",
+        choice2: "calculator",
+        choice3: "processor",
+        imgSrc: "assets/images/favicon.ico",
+        correct: "4"
+    }, {
+        question: "The first mechanical computer designed by Charles Babage was called ?",
+        choice0: "Abacus",
+        choice1: "Analytical engine",
+        choice2: "calculator",
+        choice3: "processor",
+        imgSrc: "assets/images/favicon.ico",
+        correct: "1"
+    },
 ]
 
-$(document).ready(() =>{
-    var $start = $("#start");
+//create some variables
+const lastQuestion = questions.length -1;
+let runningQuestion = 0;
 
-});
-var currentQuestion = 0;
-var score = 0;
-var totalQuestions = questions.length;
+//render a question
+function renderQuestion(){
+    let q = questions[runningQuestion];
+    q.question;
+    //question.innerHTML = q.question;
 
-console.log(questions[0]);
+}
+
+$(document).ready(() => {
+    const start = $("#start");
+    const quiz = $("#quiz");
+
+    start.on('click', () => {
+        quiz.show();
+        start.hide();
+    })
+})
+
